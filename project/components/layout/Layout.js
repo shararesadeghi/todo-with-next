d28b7274ee76e618198730bad74504b3d6556ae7
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-11-19 00:23:26 +0330

    add codes to layout

--- project/components/layout/Layout.js
+++ project/components/layout/Layout.js
@@ -1,4 +1,7 @@
-
+import Link from 'next/link';
+import {VscListSelection} from 'react-icons/vsc';
+import {BiMessageAltAdd} from 'react-icons/bi';
+import {RxDashboard} from 'react-icons/rx';
 
 const Layout = () => {
     return (
@@ -6,7 +9,25 @@ const Layout = () => {
             <header>
                 <p>Todo App</p>
             </header>
-            <div></div>
+            <div className="container--main">
+                <aside>
+                    <p>Welcome</p>
+                    <ul>
+                        <li>
+                            <VscListSelection/>
+                            <Link href="/">Todos</Link>
+                        </li>
+                        <li>
+                            <BiMessageAltAdd/>
+                            <Link href="/add-todo">Add Todo</Link>
+                        </li>
+                        <li>
+                            <RxDashboard/>
+                            <Link href="/profile">Profile</Link>
+                        </li>
+                    </ul>
+                </aside>
+            </div>
             
         </div>
     );
