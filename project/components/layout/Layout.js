68b5c7178225ab8a6867a9bea9f89140720fb01e
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-11-19 00:30:06 +0330

    create User model & npm i mongoose

--- project/components/layout/Layout.js
+++ project/components/layout/Layout.js
@@ -1,36 +1,35 @@
-import Link from 'next/link';
-import {VscListSelection} from 'react-icons/vsc';
-import {BiMessageAltAdd} from 'react-icons/bi';
-import {RxDashboard} from 'react-icons/rx';
+import Link from "next/link";
+import { VscListSelection } from "react-icons/vsc";
+import { BiMessageAltAdd } from "react-icons/bi";
+import { RxDashboard } from "react-icons/rx";
 
 const Layout = () => {
-    return (
-        <div className="container">
-            <header>
-                <p>Todo App</p>
-            </header>
-            <div className="container--main">
-                <aside>
-                    <p>Welcome</p>
-                    <ul>
-                        <li>
-                            <VscListSelection/>
-                            <Link href="/">Todos</Link>
-                        </li>
-                        <li>
-                            <BiMessageAltAdd/>
-                            <Link href="/add-todo">Add Todo</Link>
-                        </li>
-                        <li>
-                            <RxDashboard/>
-                            <Link href="/profile">Profile</Link>
-                        </li>
-                    </ul>
-                </aside>
-            </div>
-            
-        </div>
-    );
+  return (
+    <div className="container">
+      <header>
+        <p>Todo App</p>
+      </header>
+      <div className="container--main">
+        <aside>
+          <p>Welcome</p>
+          <ul>
+            <li>
+              <VscListSelection />
+              <Link href="/">Todos</Link>
+            </li>
+            <li>
+              <BiMessageAltAdd />
+              <Link href="/add-todo">Add Todo</Link>
+            </li>
+            <li>
+              <RxDashboard />
+              <Link href="/profile">Profile</Link>
+            </li>
+          </ul>
+        </aside>
+      </div>
+    </div>
+  );
 };
 
 export default Layout;
