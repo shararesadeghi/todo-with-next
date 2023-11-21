3d2fcad72dcc9fd87d2ce6949cb71fd64e68b884
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-11-22 00:43:13 +0330

    define changehandler function for next & back buttons

--- project/components/module/Tasks.js
+++ project/components/module/Tasks.js
@@ -1,21 +1,46 @@
-import {RiMastodonLine} from 'react-icons/ri';
-import {BiRightArrow,BiLeftArrow} from 'react-icons/bi';
+import { RiMastodonLine } from "react-icons/ri";
+import { BiRightArrow, BiLeftArrow } from "react-icons/bi";
 
-const Tasks = ({data, fetchTodos, next,back}) => {
-    return (
-        <div className = "tasks">
-            {data?.map(item=><div key={item._id} className="tasks__card">
-                <span className={item.status}></span>
-                <RiMastodonLine/>
-                <h4>{item.title}</h4>
-                <div>
-                    {back ?  <button className="button-back"> <BiLeftArrow/> Back</button> : null}
-                    {next ? <button className="button-next">Next <BiRightArrow/></button> : null}
-                </div>
-            </div>)}
-            
+const Tasks = ({ data, fetchTodos, next, back }) => {
+  const changeHandler = async (id, status) => {
+    const res = await fetch("/api/todos", {
+      method: "PATCH",
+      body: JSON.stringify({ id, status }),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await res.json();
+    if (data.status === "success") fetchTodos();
+  };
+  return (
+    <div className="tasks">
+      {data?.map((item) => (
+        <div key={item._id} className="tasks__card">
+          <span className={item.status}></span>
+          <RiMastodonLine />
+          <h4>{item.title}</h4>
+          <div>
+            {back ? (
+              <button
+                className="button-back"
+                onClick={() => changeHandler(item._id, back)}
+              >
+                {" "}
+                <BiLeftArrow /> Back
+              </button>
+            ) : null}
+            {next ? (
+              <button
+                className="button-next"
+                onClick={() => changeHandler(item._id, next)}
+              >
+                Next <BiRightArrow />
+              </button>
+            ) : null}
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default Tasks;
