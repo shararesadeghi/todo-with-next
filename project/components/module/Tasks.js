68d09be8347db4d9b68e71f513aa2310f97e9fae
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-11-22 00:29:23 +0330

    some props to tasks & show back and next button

--- project/components/module/Tasks.js
+++ project/components/module/Tasks.js
@@ -1,12 +1,17 @@
 import {RiMastodonLine} from 'react-icons/ri';
+import {BiRightArrow,BiLeftArrow} from 'react-icons/bi';
 
-const Tasks = ({data}) => {
+const Tasks = ({data, fetchTodos, next,back}) => {
     return (
         <div className = "tasks">
             {data?.map(item=><div key={item._id} className="tasks__card">
                 <span className={item.status}></span>
                 <RiMastodonLine/>
                 <h4>{item.title}</h4>
+                <div>
+                    {back ?  <button className="button-back"> <BiLeftArrow/> Back</button> : null}
+                    {next ? <button className="button-naxt">Next <BiRightArrow/></button> : null}
+                </div>
             </div>)}
             
         </div>
