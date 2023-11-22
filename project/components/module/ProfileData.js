31325d75ab99c618924372fa094f7adb05d0365c
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-11-22 16:47:26 +0330

    manage show information in profile

---
+++ project/components/module/ProfileData.js
@@ -0,0 +1,21 @@
+
+const ProfileData = ({data}) => {
+    return (
+        <div className="profile-data">
+            <div>
+                <span>Name:</span>
+                <p>{data.name}</p>
+            </div>
+            <div>
+                <span>Last Name:</span>
+                <p>{data.lastName}</p>
+            </div>
+            <div>
+                <span>Email:</span>
+                <p>{data.email}</p>
+            </div>
+        </div>
+    );
+};
+
+export default ProfileData;
--- project/components/template/ProfilePage.js
+++ project/components/template/ProfilePage.js
@@ -1,11 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { CgProfile } from "react-icons/cg";
+import ProfileData from "../module/ProfileData";
 import ProfileForm from "../module/ProfileForm";
 
 const ProfilePage = () => {
   const [name, setName] = useState("");
   const [lastName, setLastName] = useState("");
   const [password, setPassword] = useState("");
+  const [data, setData] = useState(null);
+
+  const fetchProfile = async ()=>{
+      const res = await fetch("/api/profile");
+      const data = await res.json();
+
+      if(data.status === "success" && data.data.name && data.data.lastName){
+          setData(data.data)
+      }
+  }
+
+  useEffect(()=>{fetchProfile()},[])
 
   const submitHandler = async () => {
     const res = await fetch("/api/profile", {
@@ -21,15 +34,18 @@ const ProfilePage = () => {
       <h2>
         <CgProfile /> Profile
       </h2>
-      <ProfileForm
-        name={name}
-        lastName={lastName}
-        password={password}
-        setName={setName}
-        setLastName={setLastName}
-        setPassword={setPassword}
-        submitHandler={submitHandler}
-      />
+     {
+         data ? <ProfileData data={data}/> :
+         <ProfileForm
+         name={name}
+         lastName={lastName}
+         password={password}
+         setName={setName}
+         setLastName={setLastName}
+         setPassword={setPassword}
+         submitHandler={submitHandler}
+       />
+     }
     </div>
   );
 };
